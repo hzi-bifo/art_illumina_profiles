1d0fe210f3f9c44f4be6f6ba259518edb3b70e28
VSack <v.sack@tu-bs.de>
2018-11-23 16:31:54 +0100

    Added conda environment requirements and download script

---
+++ download_bam_bai.py
@@ -0,0 +1,33 @@
+"""
+This script downloads bam and bai files according to the links
+
+    $ download_bam_bai.py
+    --obam {output.bam}
+    --obai {output.index}
+    --bamlink {params.bam}
+    --bailink {params.bai}
+"""
+
+import wget
+import argparse
+import os
+
+server = "http://rest.ensembl.org"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--obam', action='store', help='Name of stored bam file');
+parser.add_argument('--obai', action='store', help='Name of stored bai file');
+parser.add_argument('--bamlink', action='store', help='FTP-link to bam file');
+parser.add_argument('--bailink', action='store', help='FTP-link to bai file');
+params = parser.parse_args();
+
+bai_directory = os.path.dirname(params.obai)
+if not os.path.exists(bai_directory):
+    os.makedirs(bai_directory)
+
+wget.download(params.bailink, params.obai)
+
+#bam_directory = os.path.dirname(params.obam)
+#if not os.path.exists(bam_directory):
+#    os.makedirs(bam_directory)
+#wget.download(params.bamlink, params.obam)
